1c978e1fbacd85544dfaa559ba8e5c15953fd371
Vitor <vitorguilherme@geb.inatel.br>
2023-01-25 21:35:22 -0300

    Configurando o som de cada tecla no arquivo mains.js

---
+++ main.js
@@ -0,0 +1,20 @@
+function tocaSom (idAudio) {
+    
+    document.querySelector(idAudio).play();
+}
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    } 
+    contador = contador +1;
+}
