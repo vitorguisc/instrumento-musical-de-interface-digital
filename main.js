8616f63543bee26549e6e10253b23f08b1485d97
Vitor <vitorguilherme@geb.inatel.br>
2023-01-26 21:57:07 -0300

    Finalizando o projeto

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
-function tocaSom (idAudio) {
-    
-    document.querySelector(idAudio).play();
+function tocaSom (idAudio) {    
+    const elemento = document.querySelector(idAudio);
+
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();   
+    } else{
+        console.log('Elemento não encontrado ou inválido');
+    }
 }
 
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while(contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador ++){
     
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
+
     tecla.onclick = function(){
         tocaSom(idAudio);
     } 
-    contador = contador +1;
+
+    tecla.onkeydown = function(evento){       
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+        tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
